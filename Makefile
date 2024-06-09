0dd75617259400a7dbfaf15f61c78d4cae4ad973
Thiago L. A. Miller <tmiller@mochsl.org.br>
2024-06-08 23:17:23 -0300

    Add roles/: Create roles and policies

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 SHELL         := bash
 MAKE_DIR      := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-S3_DIR        := s3
-ECR_DIR       := ecr
+S3_DIR        := $(MAKE_DIR)s3
+ECR_DIR       := $(MAKE_DIR)ecr
+ROLES_DIR     := $(MAKE_DIR)roles
 CONFIG_FILE   ?= config.mk
 CONFIG_FILE   := $(abspath $(CONFIG_FILE))
 
@@ -12,11 +13,12 @@ export \
 	MAKE_DIR \
 	S3_DIR \
 	ECR_DIR \
+	ROLES_DIR \
 	CONFIG_FILE
 
 .PHONY: all
 
-all: s3 ecr
+all: s3 ecr roles
 
 .PHONY: s3
 
@@ -27,3 +29,8 @@ s3:
 
 ecr:
 	$(MAKE) -C $(ECR_DIR)
+
+.PHONY: roles
+
+roles:
+	$(MAKE) -C $(ROLES_DIR)
--- config.mk
+++ config.mk
@@ -1,5 +1,7 @@
-S3_BUCKET_REF   := sider-ref
-GENOME_PATH     := /home/genomes/Homo_sapiens/hg38/hg38.25chr.fa.gz
-ANNOTATION_PATH := /home/projects2/databases/gencode/release36/gencode.v36.annotation.gtf.gz
-ECR_IMAGE       := siderp
-ECR_IMAGE_TAG   := latest
+S3_BUCKET_REF    := sider-ref
+GENOME_PATH      := /home/genomes/Homo_sapiens/hg38/hg38.25chr.fa.gz
+ANNOTATION_PATH  := /home/projects2/databases/gencode/release36/gencode.v36.annotation.gtf.gz
+ECR_IMAGE        := siderp
+ECR_IMAGE_TAG    := latest
+IAM_ROLE_BATCH   := siderAWSBatchRole
+IAM_POLICY_BATCH := siderAWSBatchPolicy
