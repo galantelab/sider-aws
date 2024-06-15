cfad7f0464ead2ac92ac8fb09dbbedad2290b539
Thiago L. A. Miller <tmiller@mochsl.org.br>
2024-06-14 21:20:25 -0300

    Fix ec2/: User data as mime file and  EBS device

--- ec2/data/user_data.sh
+++ ec2/data/user_data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-mkfs -t ext4 /dev/ebs
+mkfs -t ext4 /dev/sdf
 mkdir /home/workdir
-mount /dev/ebs /home/workdir
+mount /dev/sdf /home/workdir
 chown nobody:nogroup /home/workdir
 chmod 755 /home/workdir
-echo "/dev/ebs /home/workdir ext4 defaults,nofail 0 2" >> /etc/fstab
+echo "/dev/sdf /home/workdir ext4 defaults,nofail 0 2" >> /etc/fstab
