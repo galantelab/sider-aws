a6062536e99e23a0acbd602386525b1fb86fff1f
Thiago L. A. Miller <tmiller@mochsl.org.br>
2024-06-18 00:54:52 -0300

    Fix ec2/,batch/: Write permission in container

--- ec2/data/user_data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mkfs -t ext4 /dev/sdf
-mkdir /home/workdir
-mount /dev/sdf /home/workdir
-chown nobody:nogroup /home/workdir
-chmod 755 /home/workdir
-echo "/dev/sdf /home/workdir ext4 defaults,nofail 0 2" >> /etc/fstab
